f0b64d09c2d26cc2a8706af745b22b495f4ce259
fishblish <32463704+fishblish@users.noreply.github.com>
2023-11-08 11:17:10 +0100

    Update cicero.R

--- src/R/cicero.R
+++ src/R/cicero.R
@@ -1,15 +1,6 @@
-if (!require("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-BiocManager::install(version = "3.18")
-
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-BiocManager::install("cicero")
-
-install.packages("igraph")
-
 library(cicero)
 library(igraph)
+set.seed(736)
 
 data(cicero_data)
 
@@ -17,7 +8,6 @@ input_cds <- make_atac_cds(cicero_data, binarize = TRUE)
 class(input_cds)
 
 
-set.seed(2017)
 input_cds <- detectGenes(input_cds)
 input_cds <- estimateSizeFactors(input_cds)
 
@@ -27,3 +17,14 @@ input_cds <- reduceDimension(input_cds, max_components = 2, num_dim=6,
 tsne_coords <- t(reducedDimA(input_cds))
 row.names(tsne_coords) <- row.names(pData(input_cds))
 cicero_cds <- make_cicero_cds(input_cds, reduced_coordinates = tsne_coords)
+
+data("human.hg19.genome")
+sample_genome <- subset(human.hg19.genome, V1 == "chr18")
+conns <- run_cicero(cicero_cds, sample_genome) # Takes a few minutes to run
+
+data(gene_annotation_sample)
+plot_connections(conns, "chr18", 8575097, 8839855, 
+                 gene_model = gene_annotation_sample, 
+                 coaccess_cutoff = .25, 
+                 connection_width = .5, 
+                 collapseTranscripts = "longest" )
